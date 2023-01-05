e41de44b6646bac5e8423c29d66ad1cc09c53ad9
Mateusz Kozera <Matas.wp@gmail.com>
2023-01-05 23:00:06 +0100

    Add files via upload
    update new functions

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// var a = document.createElement("a");
+
+// a.href = window.URL.createObjectURL(new Blob(["CONTENT"], {type: "text/plain"}));
+
+// a.download = "demo.txt";
+
+// a.click();
+
+// Page 1
+
+// var pageContent = document.querySelector(".card-container").innerHTML; 
+// sessionStorage.setItem("page1content", pageContent);
+
+// Page 2:
+
+// document.document.querySelector(".card-container").innerHTML=sessionStorage.getItem("page1content")
